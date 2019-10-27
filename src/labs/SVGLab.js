58df8cd236ed560bc63492d6185f150af212f98c
Sergey <soloss@yandex.ru>
2019-10-27 12:43:02 +0300

    add react-spring animation to SVG lab

--- src/labs/SVGLab.js
+++ src/labs/SVGLab.js
@@ -1,34 +1,42 @@
 import React, { useState } from 'react'
 import { createUseStyles } from 'react-jss'
+import { Spring } from 'react-spring/renderprops'
 
 const useStyles = createUseStyles({
   container: {
     padding: '1em'
   },
-  circle: {
-    fill: 'brown',
-    r: '6%',
-    transition: '.5s',
-    '&:hover': {
-      r: '6.5%'
-    }
+  piece: {
+    transition: '1s',
+    stroke: 'black',
+    strokeWidth: '2',
+    fill: 'red'
   }
 })
 
 function SVGLab() {
   const [pos, setPos] = useState({ x: 120, y: 200 })
   const classes = useStyles()
+
   return (
-    <svg viewBox='-10 -10 650 650' className={classes.container}>
-      <circle cx={40} cy={120} className={classes.circle}></circle>
-      <circle cx={120} cy={40} className={classes.circle}></circle>
-      <circle
-        cx={pos.x}
-        cy={pos.y}
-        onClick={() => setPos(prev => ({ x: prev.x + 80, y: prev.y + 80 }))}
-        className={classes.circle}
-      ></circle>
-      <circle cx={200} cy={120} className={classes.circle}></circle>
+    <svg
+      viewBox='-10 -10 650 650'
+      className={classes.container}
+      onClick={() => setPos(prev => ({ x: prev.x + 80, y: prev.y + 80 }))}
+    >
+      <defs>
+        <path
+          className={classes.piece}
+          id='blackMan'
+          d={`m -36,0 a 36,36 0 1,0 72,0 a 36,36 0 1,0 -72,0 
+          m 8,0 a 28,28 0 1,0 56,0 a 28,28 0 1,0 -56,0 
+          m 8,0 a 20,20 0 1,0 40,0 a 20,20 0 1,0 -40,0`}
+        />
+      </defs>
+      <circle cx={40} cy={120} r={36} className={classes.piece}></circle>
+      <circle cx={120} cy={40} r={36} className={classes.piece}></circle>
+      <Spring to={pos}>{props => <use href='#blackMan' {...props}></use>}</Spring>
+      <circle cx={200} cy={120} r={36} className={classes.piece}></circle>
     </svg>
   )
 }
